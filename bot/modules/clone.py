e654b292209ed2a87ca5b6a8b91b33c506d0d1a1
LFG2019347 <56392880+LFG2019347@users.noreply.github.com>
2021-02-15 16:29:52 +0800

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -1,24 +1,29 @@
 from telegram.ext import CommandHandler
-from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
-from bot import dispatcher
+from bot.helper.ext_utils.rclone_upload import rc_upload
+from bot import dispatcher, DOWNLOAD_DIR
+import os.path
 
 
 @new_thread
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
-        link = args[1]
-        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
-        gd = GoogleDriveHelper()
-        result = gd.clone(link)
-        deleteMessage(context.bot,msg)
-        sendMessage(result,context.bot,update)
+        try:
+            path = os.path.join(DOWNLOAD_DIR,args[1])
+            msg = sendMessage(f"Cloning: <code>{path}</code>",context.bot,update)
+            upload_message = rc_upload(path)
+        except Exception as e:
+            deleteMessage(context.bot,msg)
+            sendMessage(e,context.bot,update)
+        else:
+            deleteMessage(context.bot,msg)
+            sendMessage(upload_message,context.bot,update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
+        sendMessage("Provide file path to Clone.",context.bot,update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
